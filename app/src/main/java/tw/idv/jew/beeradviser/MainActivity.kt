02d56083823a510fcaec0631b062314383ccb668
a0916105 <whdofjew@gmail.com>
2022-11-17 20:57:26 +0800

    TextView的text

--- app/src/main/java/tw/idv/jew/beeradviser/MainActivity.kt
+++ app/src/main/java/tw/idv/jew/beeradviser/MainActivity.kt
@@ -3,6 +3,7 @@ package tw.idv.jew.beeradviser
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +14,8 @@ class MainActivity : AppCompatActivity() {
         findBeer.setOnClickListener {
 
         }
+
+        val brands = findViewById<TextView>(R.id.brands)
+        brands.text = "Gottle of geer"
     }
 }
